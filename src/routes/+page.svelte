0ee85df112bab3aa5ec281fbfdb0ea387ed74df3
DaMuehlthaler <muehlthaler.daniel@johak.at>
2025-01-10 11:02:55 +0100

    fetlink method

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<h1 class="text-white">Initial Commit</h1>
+<script>
+	let ip = '';
+	let fetchlink =
+		'https://geo.ipify.org/api/v2/country,city?apiKey=at_P2k1SJ7wtx0ZDGYh8UMH2PPI6Xz0w&ipAddress=';
+
+	function combineLink() {
+		let finishfetchlink = fetchlink + ip;
+		console.log(finishfetchlink);
+		console.log(ip);
+	}
+</script>
+
+<h1>IP Adress Tracker</h1>
+<input type="text" bind:value={ip} placeholder="Enter your IP" />
+<button class="btn" onclick={combineLink}>Hallihalo</button>
