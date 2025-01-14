a7c0da3fb0cb6eeb23640948ec168f924c03929d
DaMuehlthaler <muehlthaler.daniel@johak.at>
2025-01-14 10:40:53 +0100

    js update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,19 @@
 		'https://geo.ipify.org/api/v2/country,city?apiKey=at_P2k1SJ7wtx0ZDGYh8UMH2PPI6Xz0w&ipAddress=';
 
 	function combineLink() {
-		let finishfetchlink = fetchlink + ip;
-		console.log(finishfetchlink);
-		console.log(ip);
+		if (checkIfValidIp(ip) === true) {
+			console.log('valid');
+			let finishfetchlink = fetchlink + ip;
+			console.log(finishfetchlink);
+			console.log(ip);
+		} else {
+			console.log('invalid ip');
+		}
+	}
+	function checkIfValidIp(ip) {
+		const ipRegex =
+			/^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}$/;
+		return ipRegex.test(ip);
 	}
 </script>
 
