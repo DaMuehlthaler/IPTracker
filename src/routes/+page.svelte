57c2f5e8fdda83e096e6e372fc031041d3543a6f
Lukassssssssssss <ober.lukas@johak.at>
2025-01-14 11:49:22 +0100

    leaflet api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,54 +1,80 @@
 <script>
-	let ip = '';
-	let fetchlink =
-		'https://geo.ipify.org/api/v2/country,city?apiKey=at_P2k1SJ7wtx0ZDGYh8UMH2PPI6Xz0w&ipAddress=';
+  import { onMount } from 'svelte';
+  import 'leaflet/dist/leaflet.css';
+  import 'leaflet.markercluster/dist/MarkerCluster.css';
+  import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
-	function combineLink() {
-		if (checkIfValidIp(ip) === true) {
-			console.log('valid');
-			let finishfetchlink = fetchlink + ip;
-			console.log(finishfetchlink);
-			console.log(ip);
-			fetchIpData();
-		} else {
-			console.log('invalid ip');
-		}
-	}
-	function checkIfValidIp(ip) {
-		const ipRegex =
-			/^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}$/;
-		return ipRegex.test(ip);
-	}
+  let map;
+  let L;
 
-	let ipad = '';
-	let location = '';
-	let timezone = '';
-	let isp = '';
+  onMount(async () => {
+    L = await import('leaflet');
+    map = L.map('map').setView([51.505, -0.09], 13);
 
-	async function fetchIpData() {
-		try {
-			const response = await fetch(fetchlink + ip);
-			const data = await response.json();
-			console.log(data);
-			ipad = data.ip;
-			location = data.location.city + ', ' + data.location.country;
-			timezone = 'UTC' + data.location.timezone;
-			isp = data.isp;
-			console.log(ipad, location, timezone, isp);
-		} catch (error) {
-			console.log(error);
-		}
-	}
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+  });
+
+  let ip = '';
+  let fetchlink =
+    'https://geo.ipify.org/api/v2/country,city?apiKey=at_P2k1SJ7wtx0ZDGYh8UMH2PPI6Xz0w&ipAddress=';
+
+  function combineLink() {
+    if (checkIfValidIp(ip)) {
+      console.log('valid');
+      fetchIpData();
+    } else {
+      console.log('invalid ip');
+    }
+  }
+
+  function checkIfValidIp(ip) {
+    const ipRegex =
+      /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}$/;
+    return ipRegex.test(ip);
+  }
+
+  let ipad = '';
+  let location = '';
+  let timezone = '';
+  let isp = '';
+
+  async function fetchIpData() {
+    try {
+      const response = await fetch(fetchlink + ip);
+      const data = await response.json();
+      console.log(data);
+      ipad = data.ip;
+      location = data.location.city + ', ' + data.location.country;
+      timezone = 'UTC' + data.location.timezone;
+      isp = data.isp;
+
+
+      const lat = data.location.lat;
+      const lng = data.location.lng;
+      map.setView([lat, lng], 13);
+
+      L.marker([lat, lng]).addTo(map)
+        .bindPopup(`Location: ${location}`)
+        .openPopup();
+
+      console.log(ipad, location, timezone, isp);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 </script>
 
-<div class="flex flex-col items-center justify-center gap-4 ">
-	<h1>IP Adress Tracker</h1>
-	<input type="text" bind:value={ip} placeholder="Enter your IP" />
-	<button class="btn" onclick={combineLink}>Submit</button>
-	<div class="flex flex-row items-center justify-center gap-4 bg-white p-4 rounded-lg shadow-lg text-black">
-		<p>IP Address: {ipad}</p>
-		<p>Location: {location}</p>
-		<p>Timezone: {timezone}</p>
-		<p>ISP: {isp}</p>
-	</div>
+<div class="flex flex-col items-center justify-center gap-4">
+  <h1>IP Address Tracker</h1>
+  <input type="text" bind:value={ip} placeholder="Enter your IP" />
+  <button class="btn" onclick={combineLink}>Submit</button>
+  <div class="flex flex-row items-center justify-center gap-4 bg-white p-4 rounded-lg shadow-lg text-black">
+    <p>IP Address: {ipad}</p>
+    <p>Location: {location}</p>
+    <p>Timezone: {timezone}</p>
+    <p>ISP: {isp}</p>
+  </div>
+  <div id="map" class="w-80 h-48"></div>
 </div>
